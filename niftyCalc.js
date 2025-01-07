5abb5f495e252b4181378ea761670a241a1ee761
king7hills <haasjh1@gmail.com>
2025-01-06 21:27:33 -0500

    Set up html. Finished CSS styling. Set buttons as js constants.

---
+++ niftyCalc.js
@@ -0,0 +1,22 @@
+const clear = document.querySelector('#clear');
+const equal = document.querySelector('#equal');
+const divide = document.querySelector('#divide');
+const multiply = document.querySelector('#multiply');
+
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const minus = document.querySelector('#minus');
+
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const plus = document.querySelector('#plus');
+
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const enter = document.querySelector('#enter');
+
+const zero = document.querySelector('#zero');
+const decimal = document.querySelector('#decimal');
