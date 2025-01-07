f027f0c075825032541b7e98a5e23641104bb888
king7hills <haasjh1@gmail.com>
2025-01-07 11:49:13 -0500

    building out js functions. changed equal to plus/minus

--- niftyCalc.js
+++ niftyCalc.js
@@ -1,5 +1,39 @@
+function add (a, b) {
+    return a + b;
+};
+
+function subtract (a, b) {
+    return a - b;
+};
+
+function multiply (a, b) {
+    return a*b;
+};
+
+function divide (a, b) {
+    return a/b;
+};
+
+function plusMinus (a) {
+    return a*-1;
+}
+
+function operate (operator, a, b) {
+    if (operator == '+') {
+       return add(a, b);
+    } else if (operator == '-') {
+       return subtract(a,b);
+    } else if (operator == '*') {
+       return multiply(a,b);
+    } else if (operator == '/') {
+       return divide(a,b);
+    }
+}
+const dispArr = [];
+
 const clear = document.querySelector('#clear');
-const equal = document.querySelector('#equal');
+clear.addEventListener("click", )
+const plusminus = document.querySelector('#plusminus');
 const divide = document.querySelector('#divide');
 const multiply = document.querySelector('#multiply');
 
